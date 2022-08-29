8263573fffe5bccc773a6b76d3937830bd93fb23
Nikolay <kolyania1@gmail.com>
2022-08-29 18:50:02 +0300

    initial commit

---
+++ resources/js/Pages/Tt.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function Tt(props) {
+    console.log(props);
+    return (
+        <h1>Tt class</h1>
+    );
+}
