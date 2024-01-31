1e2e7761869a579396281dfd06863c5a8730bc21
Nikolay <kolyania1@gmail.com>
2024-01-31 13:32:41 +0300

    hot fix

--- frontend/src/app/[lang]/user/admin/cache-clear/page.tsx
+++ frontend/src/app/[lang]/user/admin/cache-clear/page.tsx
@@ -2,8 +2,7 @@ import { revalidatePath } from "next/cache"
 import "server-only"
 
 export default async function CacheClearPage(){
-    if (window.confirm("Сбросить кеш?"))
-        revalidatePath("/", "page");
+    revalidatePath("/", "page");
 
     return <main>Done.</main>
 }
